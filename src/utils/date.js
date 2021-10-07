e7681a968780fba98ba5ac858ed32e0bffc98357
Hyunjae Kim <peterhyunjae@naver.com>
2021-10-08 04:06:19 +0900

    ⚡️ 분이 한자리일 경우 앞에 0 넣는 디테일

--- src/utils/date.js
+++ src/utils/date.js
@@ -2,7 +2,11 @@ function dateToString(timestamp) {
   const obj = new Date(timestamp);
 
   const hour = obj.getHours().toString();
-  const min = obj.getMinutes().toString();
+  let min = obj.getMinutes().toString();
+
+  if (0 <= min && min < 10) {
+    min = '0' + min;
+  }
 
   if (hour <= 12) {
     return '오전 ' + hour + ':' + min;
