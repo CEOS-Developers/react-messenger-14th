583753457241702ba253368a530e14bfde65553b
TaeeunKim <xodms0309@naver.com>
2021-10-08 02:26:30 +0900

    style: sidebar styling

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
+import { BsFillPersonFill, BsFillChatFill } from 'react-icons/bs';
+import { FiMoreHorizontal } from 'react-icons/fi';
 function SideBar() {
   return (
     <Wrapper>
-      <div>친구목록</div>
-      <div>채팅</div>
-      <div>더보기</div>
+      <BsFillPersonFill size={30} style={style} />
+      <BsFillChatFill size={25} style={style} />
+      <FiMoreHorizontal size={25} style={style} />
     </Wrapper>
   );
 }
+const style = {
+  'margin-top': '20px',
+  size: '25',
+  color: '#949494',
+};
 const Wrapper = styled.div`
   width: 100px;
   height: 100vh;
-  background: lightgray;
+  background: #e8e8e8;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 export default SideBar;
