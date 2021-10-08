595ed565106f76759f900cc1fd980de9f44c9b29
kiuk <ugiya0628@icloud.com>
2021-10-08 17:04:32 +0900

    [Feat] Route 임포트만..

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
 
-import CoupleChatRoomHeader from './couple-chat-room/components/CoupleChatRoomHeader';
-import ChatRoomMessages from './@shared/components/ChatRoomMessages';
-import ChatRoomMessageForm from './@shared/components/ChatRoomMessageForm';
+import Route from './@shared/hoc/Route';
+import Header from './@shared/components/Header';
+import ChatRoomMessages from './chat-room/components/ChatRoomMessages';
+import ChatRoomMessageForm from './chat-room/components/ChatRoomMessageForm';
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
+      <Header></Header>
       <RootContainer>
-        <CoupleChatRoomHeader></CoupleChatRoomHeader>
         <ChatRoomMessages></ChatRoomMessages>
         <ChatRoomMessageForm></ChatRoomMessageForm>
       </RootContainer>
@@ -23,28 +24,32 @@ const GlobalStyle = createGlobalStyle`
         outline: none;
     }
     body {
-        margin: 0;
-        
-        width: 100vw;
+        width: 100vw; 
         height: 100vh;
+
+        margin: 0;
         
         display: flex;
         justify-content: center;
         align-items: center;
-        background: #2ec1ac;
+
+        background: rgb(34,193,195);
+        background: linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%);
     }
 `;
 
 const RootContainer = styled.div`
   width: 550px;
-  height: 900px;
+  height: 800px;
 
-  box-shadow: 0 0 25px rgba(0, 0, 0, 0.25);
-  background: white;
+  border-radius: 30px;
 
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+
+  box-shadow: 0 0 25px rgba(0, 0, 0, 0.25);
+  background: white;
 `;
 
 export default App;
